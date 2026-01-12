e8c3b983f6cc34f0d740b72702e369967bc92d93
Tchyvio <fabian.honecker2@gmail.com>
2026-01-12 08:35:00 +0000

    typen test

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -26,10 +26,26 @@
 */
 
 
+let test; // Deklaration
 
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
 
+test = "hi";
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
 
+test = 22;
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
 
+test = false;
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
 
 /* Variablen vs. Konstanten in JS */
 
