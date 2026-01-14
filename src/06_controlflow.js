dde88535642787c57b21c6db13279b0c61fc27d2
Tchyvio <fabian.honecker2@gmail.com>
2026-01-14 10:43:32 +0000

    06 Fallunterscheidung ++

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,15 +1,15 @@
 // Entscheidungsstrukturen | control flow
 
-// Deklaration + Assignment
-const ageJohn = 30;
-const ageMark = 30;
+// // Deklaration + Assignment
+// const ageJohn = 30;
+// const ageMark = 30;
 
-// Deklaration
-let isJohnOlder, isJohnEqual;
+// // Deklaration
+// let isJohnOlder, isJohnEqual;
 
-// Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+// // Test | Logische Aussage
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
 // console.log("ageJohn: " + ageJohn);
@@ -30,11 +30,62 @@ isJohnEqual = (ageJohn == ageMark);
 // mit Alternative  
 // entweder ja oder nein
 
-if (true)
-{
-    console.log("John ist älter.");
-}
-else
-{
-    console.log("John ist jünger.");
-}
+// if (true)
+// {
+//     console.log("John ist älter.");
+// }
+// else
+// {
+//     console.log("John ist jünger.");
+// }
+
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+// const firstName = "Jane";
+// let job;
+
+// job = "driver";      // .. fährt TAXI! / UBER
+// job = "diver";       // .. taucht im Rhein!
+// job = "artist";      // .. malt ein Bild!
+// job = "pilot";       // .. macht etwas anderes! --> default
+// job = "teacher";     // .. unterrichtet!
+// job = "instructor";  // .. unterrichtet!
+
+// switch(job)
+// {
+//     case "driver":
+//         console.log(firstName + " fährt TAXI! / UBER");
+//         break;
+//     case "diver":
+//         console.log(firstName + " taucht im Rhein");
+//         break;
+
+
+
+
+
+
+
+
+//     default:
+//         console.log(firstName + " macht etwas anderes!");
+//         break;
+// }
+
+
+/******** Fallunterscheidung / SWITCH|CASE 2 *******/
+
+const cond = true;
+const value = 10;
+
+switch (cond) {
+    case (value == 1):
+        console.log("case 1.");
+        break;
+    case (value == 2):
+        console.log("case 2.");
+        break;
+    default:
+        console.log("Value hat einen anderen Wert.");
+        break;
